d00e4b3ae57a5f0c5d1aabfb25cc0c89dac7a6e4
arcnmx <arcnmx@users.noreply.github.com>
2019-05-31 12:26:08 -0700

    totp field

--- defaults.py
+++ defaults.py
@@ -18,13 +18,24 @@ FIELD_DEFAULTS = {
     'type': 'login'
 }
 
+def rel(base, path):
+    return os.path.dirname(path).replace(base, '').lstrip('/')
+
+def password(data):
+    return data.split("\n")[0]
+
+# export fields one per line in "key: value" format
+def convert_fields(fields):
+    return '\n'.join(['{}: {}'.format(k, v) for k, v in fields.items()])
+
 FIELD_FUNCTIONS = {
     'name': lambda base, path, data: os.path.basename(path),
-    'folder': lambda base, path, data: os.path.dirname(path).replace(base, '').lstrip('/'),
-    'login_password': lambda base, path, data: data.split("\n")[0],
+    'folder': lambda base, path, data: rel(base, path),
+    'login_password': lambda base, path, data: password(data),
 }
 
 FIELD_PATTERNS = {
     'login_uri': '^url ?: ?(.*)$',
     'login_username': '^user.* ?: ?(.*)$',
+    'login_totp': '^otpauth://totp/.*secret=([a-zA-Z0-9]+).*$',
 }
